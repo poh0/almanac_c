ebafc9d9cfb5c5cd3a5670f45bfcdec05164dfc6
Lauri Pöhö <xzeusking@gmail.com>
2022-07-22 02:25:57 +0300

    Add days_in_month, get_weekday etc...

--- main.c
+++ main.c
@@ -2,84 +2,73 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
+#include <math.h>
 
 #define RESET   "\033[0m"
-#define GREEN   "\033[32m"      /* Green */
-#define BOLDWHITE   "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW   "\033[1m\033[33m"      /* Bold Yellow */
 
-typedef enum {
-    JAN = 0,
-    FEB,
-    MAR,
-    APR,
-    MAY,
-    JUN,
-    JUL,
-} Month; 
+const char* const MONTHS[] = { 
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec" 
+};
 
-typedef enum {
-    MON = 0,
-    TUE,
-    WED,
-    THU,
-    FRI,
-    SAT,
-    SUN
-} Weekday; 
+const char* const WEEKDAYS[] = {
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat"
+};
 
 typedef struct {
     bool is_sig;
     bool is_current;
-    size_t index;
+    size_t mday;
 } Date;
 
 typedef struct {
     Date *date;
     const char *note; // eg. "Mike's birthday 13:00"
-} Sig_date;
+} Sig_Date;
 
 typedef struct {
-    Month currMonth;
-    Weekday firstWeekday;
-    size_t cnt_dates; 
+    size_t first_weekday;
+    size_t curr_month;
+    size_t curr_year;
     size_t curr_date;
-    Sig_date *sig_dates;
+    size_t cnt_dates; 
+    Sig_Date sig_dates[32];
     Date dates[32];
 } Calendar; 
 
-const char *weekday_to_cstr(Weekday weekday) {
-    switch (weekday) {
-        case MON:
-            return "MON";
-        case TUE:
-            return "TUE";
-        case WED:
-            return "WED";
-        case THU:
-            return "THU";
-        case FRI:
-            return "FRI";
-        case SAT:
-            return "SAT";
-        case SUN:
-            return "SUN";
-        default:
-            printf("Unreachable\n");
-            return NULL;
-    };
-}
-
-void print_sig_date(Sig_date *sig_date) {
+void print_sig_date(Sig_Date *sig_date) {
     (void) sig_date;
 } 
 
 void print_calendar(Calendar *calendar) {
 
-    size_t begin_day_idx = calendar->firstWeekday;
+    size_t begin_day_idx = calendar->first_weekday;
 
-    printf("          Jan 2022\n");
-    for (size_t i = MON; i <= SUN; i++) {
-        printf("%4s", weekday_to_cstr(i));
+    // Print month and year
+    printf("          %s %zu\n", MONTHS[calendar->curr_month], calendar->curr_year);
+
+    // Print weekdays
+    for (size_t i = 0; i < 7; i++) {
+        printf("%4s", WEEKDAYS[i]);
     }
  
     printf("\n");
@@ -89,8 +78,8 @@ void print_calendar(Calendar *calendar) {
         printf("%4s", " ");
     }
 
-
     size_t current_idx = 0;
+
     while (current_idx < calendar->cnt_dates) {
         /* If we are on sunday, newline */
         if (begin_day_idx == 7) {
@@ -98,15 +87,22 @@ void print_calendar(Calendar *calendar) {
             printf("\n");
             continue;
         }
-        else if (current_idx == calendar->curr_date) {
-            printf(GREEN "%4zu" RESET, current_idx++);
+
+        else if (current_idx + 1 == calendar->curr_date) {
+            if (calendar->dates[current_idx].is_sig)
+                printf(BOLDGREEN "%3zu!" RESET, ++current_idx);
+            else
+                printf(BOLDGREEN "%4zu" RESET, ++current_idx);
         }
+
         else if (calendar->dates[current_idx].is_sig) {
-            printf(BOLDWHITE "%4zu" RESET, current_idx++);
+            printf(BOLDYELLOW "%4zu" RESET, ++current_idx);
         }
+
         else {
-            printf("%4zu", current_idx++);
+            printf("%4zu", ++current_idx);
         }
+
         begin_day_idx++;
     }
     printf("\n");
@@ -116,24 +112,68 @@ void populate_dates(Calendar *cal) {
     Date *date = NULL;
     for (size_t i = 0; i < cal->cnt_dates; i++) {
         date = &cal->dates[i];
-        date->index = i;
-        date->is_current = (date->index == cal->curr_date);
+        date->mday = i + 1;
+        date->is_current = (date->mday == cal->curr_date);
         date->is_sig = false;
     }
-    // Here for reference
-    cal->dates[13].is_sig = true;
+    // Mock data
+    cal->dates[2].is_sig = true;
+}
+
+// we take time difference in seconds from
+// current month's first day to next month's first day
+size_t days_in_month(struct tm time_now) {
+
+    struct tm next_month = { 0 };
+    next_month.tm_year = time_now.tm_year;
+    next_month.tm_mon = time_now.tm_mon + 1;
+    next_month.tm_mday = 1;
+    next_month.tm_isdst = -1;
+
+    struct tm this_month = { 0 };
+    this_month.tm_year = time_now.tm_year;
+    this_month.tm_mon = time_now.tm_mon;
+    this_month.tm_mday = 1;
+    this_month.tm_isdst = -1;
+
+    time_t next_month_t = mktime(&next_month);
+    time_t this_month_t = mktime(&this_month);
+
+    double dDays = difftime(next_month_t, this_month_t) / 86400;
+    size_t days = (size_t) dDays;
+    return days;
+}
+
+size_t get_weekday(struct tm time_now) {
+
+    struct tm this_month = { 0 };
+    this_month.tm_year = time_now.tm_year;
+    this_month.tm_mon = time_now.tm_mon;
+    this_month.tm_mday = 1;
+    this_month.tm_isdst = -1;
+
+    mktime(&this_month);
+    return this_month.tm_wday;
 }
 
 void init_calendar(Calendar *cal) {
 
-    /* TODO: actual current month & date */
+    time_t t = time(NULL);
+    struct tm time_now = *localtime(&t);
 
-    cal->cnt_dates = 30;
-    cal->curr_date = 22;
+//  struct tm time_now = { 0 };
+//  time_now.tm_year = 2005 - 1900;
+//  time_now.tm_mon = 6;
+//  time_now.tm_mday = 24;
+//  time_now.tm_isdst = -1;
+
+    cal->curr_year      = time_now.tm_year + 1900;
+    cal->curr_month     = time_now.tm_mon;
+    cal->curr_date      = time_now.tm_mday;
+    cal->cnt_dates      = days_in_month(time_now);
+    cal->first_weekday  = get_weekday(time_now);
+
     populate_dates(cal);
-    cal->firstWeekday = WED;
-    cal->currMonth = JUL;
-    cal->sig_dates = NULL;
 }          
 
 int main(int argc, char** argv) {
